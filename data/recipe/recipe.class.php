247cc6d327dde538ae880f4add218ec176bc4bb4
Benjamin Lorteau <ben.lort@gmail.com>
2013-06-23 15:43:49 -0400

    Add get_gather_list function

--- data/recipe/recipe.class.php
+++ data/recipe/recipe.class.php
@@ -10,7 +10,15 @@ class Recipe extends Recipe_Model {
 
   // CUSTOM
 
-  //Custom content
+  public static function get_gather_list(){
+	  $sql = '
+SELECT r.*
+FROM `'.self::get_table_name().'` r
+LEFT JOIN `recipe_consumable` r_c ON r_c.`recipe_id` = r.`id`
+WHERE r_c.`recipe_id` IS NULL';
+
+    return self::sql_to_list($sql);
+  }
 
   // /CUSTOM
 
